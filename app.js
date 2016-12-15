bef2138709ea56e834e812456b87b132a37f7c6a
Mariam Qasim <mariam.a.qasim@gmail.com>
2016-12-15 16:04:15 -0500

    going to be conflict

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ function init(){
 }
 
 function comment(){
-  let lets_chat = document.querySelector('.lets_chat')
+  let lets_chat = document.querySelector('#chat')
   let item = document.querySelector('.item');
   let parent = document.querySelector('.chatbox');
 
   item.textContent = lets_chat.textContent;
   parent.appendChild(item);
+
+  lets_chat.textContent = " ";
 
 }
 
