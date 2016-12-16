fd30b31fbd40712fe570b4b2daceffd83567a5ca
Mariam Qasim <mariam.a.qasim@gmail.com>
2016-12-16 15:57:59 -0500

    mostly done, still need to get data on a loop

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
+let user = "";
+let message = "";
+
 
 function init(){
   let submit = document.querySelector('#submit');
-  submit.addEventListener('click', getMessage);
+  submit.addEventListener('click', sendMessage);
 }
 
 
-function comment(x, y){
-  let lets_chat = document.querySelector('input');
+function comment(user, message){
+  let lets_chat = document.querySelector('#message');
   let sender = document.createElement('h3');
   let item = document.createElement('p');
   let parent = document.querySelector('.chatbox');
-  
+  let section = document.querySelector('section');
 
-  item.textContent = lets_chat.value;
-  parent.appendChild(item)
-  
-  parent.appendChild(sender);
-  parent.appendChild(item);
-  sender.textContent = x;
-  item.textContent = y;
-  
-  
 
+  item.textContent = "Me: " + lets_chat.value;
+  parent.appendChild(section);
+
+  section.appendChild(sender);
+  section.appendChild(item);
+  sender.textContent = user;
+  item.textContent = message;
 
 
   lets_chat.value = '';
@@ -36,17 +37,41 @@ function getMessage() {
     let response = JSON.parse(request.responseText);
     console.log(response);
 
-    console.log(response.chats[0].from);
-    console.log(response.chats[0].message);
+////trying for loop here
 
-    let user = response.chats[0].from;
-    let message = response.chats[0].message;
+  for (let i = 0; i < response.chats.length; i++) {
+    console.log(response.chats[i].from);
+    console.log(response.chats[i].message);
 
-    comment(user, message);
-
+    user = response.chats[i].from;
+    message = response.chats[i].message;
+    }
   });
+
+  comment(user, message);
   request.send();
   console.log("request sent");
+
+}
+
+
+function sendMessage(){
+  let request = new XMLHttpRequest();
+  request.open('POST', 'http://api.queencityiron.com/chats');
+
+  let body = JSON.stringify({
+    from: document.querySelector("#username").value,
+    message: document.querySelector("#message").value,
+  })
+
+  let user = document.querySelector("#username").value;
+  let message = document.querySelector("#message").value;
+
+  comment(user, message);
+
+  console.log(body);
+  request.send(body);
+
 }
 
 
